d4b7ff90f284632d9f941505c00fa7428aaa733c
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-05-07 16:09:53 -0700

    Programmed a server for some basic API's

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Robot-Automation
-Robot automation in a virtual world
-A robot that is in a virtual world.
-It picks up and drops off parcels till all the parcels have been dropped off.
-Will eventually make the picking up and dropping off process optimal by employing some algorithms later.
+
+Learning nodejs to become a great backend engineer
---
+++ nodejs/routing.js
@@ -0,0 +1,56 @@
+const http = require("http");
+
+const port = process.env.PORT || 1337;
+
+function respondText(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hey there, welcome to my website");
+}
+
+function respondJson(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      text: "Emeka",
+      obj: {
+        date: new Date(),
+        id: 1234,
+        cause: "I do not know",
+      },
+    })
+  );
+}
+
+function respondSalinha(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hi Salinha, I love you forever");
+}
+
+function responseNotFound(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Not Found");
+}
+
+function careers_swe(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("You have been given the position of software engineer at stripe");
+}
+
+const server = http.createServer(function (req, res) {
+  // use a switch statement to send response based on the url
+  switch (req.url) {
+    case "/":
+      return respondText(req, res);
+    case "/json":
+      return respondJson(req, res);
+    case "/salinha":
+      return respondSalinha(req, res);
+    case "/careers/swe":
+      return careers_swe(req, res);
+    default:
+      responseNotFound(req, res);
+  }
+});
+
+server.listen(port);
+console.log(`Server is listening on port ${port}`);
