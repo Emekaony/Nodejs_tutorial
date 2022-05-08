ec2f7ecbb9eb0fe1959916ed344c4c4c6e0d5355
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-05-07 23:36:22 -0700

    some file structure minimal chamges

--- app.js
+++ app.js
@@ -1,17 +1,82 @@
-// const http = require("http");
+const http = require("http");
+const querystring = require("querystring");
+const fs = require("fs");
 
-// const port = process.env.PORT || 1337;
+const PORT = process.env.PORT || 1337;
 
-// // send a JSON response back to the user
-// const server = http.createServer(function (req, res) {
-//   // set the contentType so the consumer of your API
-//   // knows what kind of data they are dealing with
-//   // this will also allow them to avoid handling it in unexpected ways
-//   res.setHeader("Content-Type", "application/json");
-//   res.end(JSON.stringify({ text: "hi", numbers: [1, 2, 3] }));
-// });
+function respondEcho(req, res) {
+  // querystring is deprecated, but it works.
+  // will adopt URLSearchParams at a later time
+  console.log(req.url.split("?"));
+  const { age = "", input = "" } = querystring.parse(
+    req.url.split("?").slice(1).join("")
+  );
 
-// server.listen(port);
-// console.log(`Server listening on port ${port}`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      normal: input,
+      shouty: input.toUpperCase(),
+      characterCount: input.length,
+      // reverse here actually alters the original input
+      // but it is not a issue now because we will not use the original input elsewhere
+      backwards: input.split("").reverse().join(""),
+      age: age,
+    })
+  );
+}
 
-console.log(__dirname);
+function respondText(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hey there, welcome to my website");
+}
+
+function respondJson(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      text: "Emeka",
+      obj: {
+        date: new Date(),
+        id: 1234,
+        cause: "I do not know",
+      },
+    })
+  );
+}
+
+function respondSalinha(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("What's up");
+}
+
+function responseNotFound(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Not Found");
+}
+
+function careers_swe(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("You have been given the position of software engineer at stripe");
+}
+
+function respondStatic(req, res) {
+  console.log(req.url.split("/static"));
+  console.log(req.url);
+  const filename = `${__dirname}/public${req.url.split("/static")[1]}`;
+  fs.createReadStream(filename)
+    .on("error", () => responseNotFound(req, res))
+    .pipe(res);
+}
+
+const server = http.createServer(function (req, res) {
+  if (req.url.match(/^\/echo/)) return respondEcho(req, res);
+  if (req.url == "/") return respondText(req, res);
+  if (req.url == "/json") return respondJson(req, res);
+  if (req.url.match(/^\/static/)) return respondStatic(req, res);
+
+  responseNotFound(req, res);
+});
+
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
