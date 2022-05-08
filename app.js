a2a603a8351a03a9f380749d210995020e3a24a1
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-05-08 01:16:15 -0700

    Done implementing the basics of the chat application

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-const http = require("http");
-const querystring = require("querystring");
 const fs = require("fs");
+const express = require("express");
+const EventEmitter = require("events");
 
+const chatEmitter = new EventEmitter();
 const PORT = process.env.PORT || 1337;
+
+const app = express();
+
+app.get("/", respondText);
+app.get("/json", respondJson);
+app.get("/echo", respondEcho);
+app.get("/static/*", respondStatic);
+app.get("/chat", respondChat);
+app.get("/sse", respondSSE);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 function respondEcho(req, res) {
   // querystring is deprecated, but it works.
   // will adopt URLSearchParams at a later time
-  console.log(req.url.split("?"));
-  const { age = "", input = "" } = querystring.parse(
-    req.url.split("?").slice(1).join("")
-  );
+  //   console.log(req.url.split("?"));
+  //   const { age = "", input = "" } = querystring.parse(
+  //     req.url.split("?").slice(1).join("")
+  //   );
 
-  res.setHeader("Content-Type", "application/json");
-  res.end(
-    JSON.stringify({
-      normal: input,
-      shouty: input.toUpperCase(),
-      characterCount: input.length,
-      // reverse here actually alters the original input
-      // but it is not a issue now because we will not use the original input elsewhere
-      backwards: input.split("").reverse().join(""),
-      age: age,
-    })
-  );
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.end(
+  //     JSON.stringify({
+  //       normal: input,
+  //       shouty: input.toUpperCase(),
+  //       characterCount: input.length,
+  //       // reverse here actually alters the original input
+  //       // but it is not a issue now because we will not use the original input elsewhere
+  //       backwards: input.split("").reverse().join(""),
+  //       age: age,
+  //     })
+  //   );
+
+  // using express:
+  const { input = "", age = "" } = req.query;
+
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    characCount: input.length,
+    backwards: input.split("").reverse().join(""),
+    age: age,
+  });
 }
 
 function respondText(req, res) {
@@ -33,16 +56,12 @@ function respondText(req, res) {
 
 function respondJson(req, res) {
   res.setHeader("Content-Type", "application/json");
-  res.end(
-    JSON.stringify({
-      text: "Emeka",
-      obj: {
-        date: new Date(),
-        id: 1234,
-        cause: "I do not know",
-      },
-    })
-  );
+  res.json({
+    text: "Emeka",
+    date: new Date(),
+    id: 1234,
+    cause: "I do not know",
+  });
 }
 
 function respondSalinha(req, res) {
@@ -61,22 +80,38 @@ function careers_swe(req, res) {
 }
 
 function respondStatic(req, res) {
-  console.log(req.url.split("/static"));
-  console.log(req.url);
-  const filename = `${__dirname}/public${req.url.split("/static")[1]}`;
+  const filename = `${__dirname}/public/${req.params[0]}`;
   fs.createReadStream(filename)
     .on("error", () => responseNotFound(req, res))
     .pipe(res);
 }
 
-const server = http.createServer(function (req, res) {
-  if (req.url.match(/^\/echo/)) return respondEcho(req, res);
-  if (req.url == "/") return respondText(req, res);
-  if (req.url == "/json") return respondJson(req, res);
-  if (req.url.match(/^\/static/)) return respondStatic(req, res);
+function respondChat(req, res) {
+  const { message } = req.query;
 
-  responseNotFound(req, res);
-});
+  chatEmitter.emit("message", message);
+  res.end();
+}
 
-server.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
+function respondSSE(req, res) {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+  });
+
+  const onMessage = (msg) => res.write(`data: ${msg}\n\n`);
+  chatEmitter.on("message", onMessage);
+
+  res.on("close", function () {
+    chatEmitter.off("message", onMessage);
+  });
+}
+
+// const server = http.createServer(function (req, res) {
+//   if (req.url.match(/^\/echo/)) return respondEcho(req, res);
+//   if (req.url == "/") return respondText(req, res);
+//   if (req.url == "/json") return respondJson(req, res);
+//   if (req.url.match(/^\/static/)) return respondStatic(req, res);
+
+//   responseNotFound(req, res);
+// });
